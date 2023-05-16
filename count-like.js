42b23556503e11798489a3ffb98b271c7f81924b
Krrithik Ezhilarasan <krrithikezhil30@gmail.com>
2023-05-16 20:36:26 +0530

    implemented count-like function

---
+++ count-like.js
@@ -0,0 +1,58 @@
+/* You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+[]                                -->  "no one likes this"
+["Peter"]                         -->  "Peter likes this"
+["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+Note: For 4 or more names, the number in "and 2 others" simply increases. */
+
+// names: array of strings
+
+/* output = ``;
+let displayText;
+
+function countLike(names) {
+  if (!Array.isArray(names)) throw new Error("Incorrect input. Only array of strings are accepted")
+  else{
+    displayText();
+ }
+    console.log(names);
+}
+
+function displayText(){
+  if(names.length=0){ `no one likes this`;
+ }else if(names.length=1){console.log(`${(names[0])} likes this`);
+ }else if(names.length=2){console.log(`${(names[0])} and ${(names[1])} like this`);
+ }else if(names.length=3){console.log(`${(names[0])}, ${(names[2])} and ${(names[3])} like this`);
+ }else if(names.length>=4){console.log(`${(names[0])},${(names[1])} and ${names.length-2} others like this`);
+  }
+   }
+
+names = ["loki", "steve"];
+
+displayText();
+const result = countLike(names);
+console.log(result); */
+
+function countLike(names) {
+  if (!Array.isArray(names))
+    throw new Error("Incorrect input. Only array of strings are accepted");
+  else if ((names.length === 0)) {
+    return `no one likes this`;
+  } else if ((names.length === 1)) {
+    return `${names[0]} likes this`;
+  } else if ((names.length === 2)) {
+    return `${names[0]} and ${names[1]} like this`;
+  } else if ((names.length === 3)) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  } else if (names.length >= 4) {
+    return `${names[0]},${names[1]} and ${names.length - 2} others like this`;
+  }
+}
+
+let names = ["loki", "steve", "travis", "david"];
+let result = countLike(names);
+console.log(result);
--- disemvowel-trolls.js
+++ disemvowel-trolls.js
@@ -1,22 +1,20 @@
 function disemvowelTrolls(inputString) {
-  let output = '';
   const vowels = ["a", "e", "i", "o", "u"];
-  const inputArray = inputString.split("");
+  const inputArray = inputString.toLowerCase().split("");
+  // const inputArray = inputString.split("");
+  let output = "";
+
   for (let index = 0; index < inputArray.length; index++) {
     const inputCharacter = inputArray[index];
-    switch (inputCharacter) {
-      case "a":
-      case "e":
-      case "i":
-    case "o":
-      case "u":
-        break;
-      default:
-        output +=inputCharacter
-        break;
+    if (!vowels.includes(inputCharacter)) {
+      output += inputCharacter;
     }
   }
+
   return output;
 }
-const result = disemvowelTrolls('kumanan');
-console.log(result);
+
+
+const result = disemvowelTrolls("kumanan");
+const result1 = disemvowelTrolls('Ezhil')
+console.log(result, result1); 
