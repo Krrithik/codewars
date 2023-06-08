b7a3499eca3e1052693b9adb2f15e74d8014ee37
Krrithik Ezhilarasan <krrithikezhil30@gmail.com>
2023-06-08 18:22:52 +0530

    draft1

--- digital-root.js
+++ digital-root.js
@@ -7,3 +7,42 @@ Examples
    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2 */
+
+
+function digitalRoot(inputNumber) {
+    let remainder;
+    let sum = 0;
+    while (inputNumber>0) {
+      remainder = inputNumber % 10;
+      sum += remainder;
+      inputNumber = Math.floor(inputNumber / 10);
+    }
+    return sum;
+  }
+  
+  let inputNumber = 2568;
+  let result = digitalRoot(inputNumber);
+  console.log(result);
+ 
+
+
+/*  
+  function digitalRoot(inputNumber) {
+    if (inputNumber < 10) {
+      return inputNumber;
+    } else {
+      let remainder;
+      let sum = 0;
+      while (inputNumber > 0) {
+        remainder = inputNumber % 10;
+        sum += remainder;
+        inputNumber = Math.floor(inputNumber / 10);
+      }
+    }
+  }
+  
+  let inputNumber = 2568;
+  let result = digitalRoot(inputNumber);
+  console.log(result); */
+
+
